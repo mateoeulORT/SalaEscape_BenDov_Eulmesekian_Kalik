81a345bcb6f825092c95a325a785dab3d6d62e6d
mateoeulORT <48044168@est.ort.edu.ar>
2024-07-27 17:22:32 -0300

    habitacion 1

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -30,27 +30,41 @@ public class HomeController : Controller
             return View("Habitacion" + Escape.GetEstadoJuego());
         }
 
-        if (Escape.ResolverSala(sala, clave) && Escape.GetEstadoJuego() == 5)
+        bool respuestaCorrecta = Escape.ResolverSala(sala, clave);
+    
+        if (respuestaCorrecta)
         {
-            return View("Victoria");
-        } 
-        else if(Escape.ResolverSala(sala, clave))
-        {
-            return View("Habitacion" + Escape.GetEstadoJuego());
+            if (Escape.GetEstadoJuego() > 5) 
+            {
+                return View("Victoria"); 
+            }
+            else
+            {
+                return View("Habitacion" + Escape.GetEstadoJuego());
+            }
         }
-        else{
-            ViewBag.Error = "Datos mal ingresados";
-            return View("Habitacion" + Escape.GetEstadoJuego());
+        else
+        {
+
+            ViewBag.Error = "La respuesta es incorrecta. Intenta nuevamente.";
+            return View("Habitacion" + sala); 
         }
 
-
-    }
+        }
     public IActionResult Comenzar()
     {
         ViewBag.EstadoJuego = Escape.GetEstadoJuego();
         return View("Habitacion" + ViewBag.EstadoJuego);
     }
     
+    public IActionResult MostrarCarta()
+    {
+        ViewBag.Parrafo1 = "<p>Compañero de celda,</p>";
+        ViewBag.Parrafo2 = "<p>La luz de la luna se cuela por los barrotes, y con ella viene un mensaje oculto. En el eco de los pasos del guardia, cada golpe de su bastón contra el suelo es una clave. Un golpe fuerte es un 1, un golpe suave es un 0.</p>";
+        ViewBag.Parrafo3 = "<p>Por ejemplo, tres golpes fuertes seguidos de tres golpes suaves te darán la clave para nuestra fuga. Descifra la secuencia de los golpes y conviértela a ese formato antiguo que simboliza el número 16. Solo así podrás abrir la puerta de nuestra libertad.</p>";
+        ViewBag.Parrafo4 = "<p>Con esperanza, Tu confidente </p>";
+        return View("Habitacion1");
+    }
 
 
 }
--- Models/Escape.cs
+++ Models/Escape.cs
@@ -2,31 +2,31 @@ class Escape{
     private static string[] IncognitasSalas {get; set;}
     private static int EstadoJuego{get; set;} = 1;
     const int cantSalas = 5;
+
     private static void InicializarJuego(){
-        IncognitasSalas = new string[cantSalas];
-        for (int i = 0; i < IncognitasSalas.Length; i++)
-        {
-            IncognitasSalas[i] = " ";
-        }
+        IncognitasSalas = new string[cantSalas] {"4F", "a", "a", "a", "a"};
     }    
+
     public static int GetEstadoJuego() {
         return EstadoJuego;
     }
     public static bool ResolverSala(int Sala, string Incognita){
-        if(IncognitasSalas.Length == 0)
+        if (IncognitasSalas == null || IncognitasSalas.Length == 0)
+        {
             InicializarJuego();
+        }
 
         if(EstadoJuego < Sala)
         {
             return false;
         }
-        else{
-            if(Incognita == IncognitasSalas[EstadoJuego]){
-                EstadoJuego++;
-                return true;
-            }
-            else
-                return false;
+        
+        if(Incognita == IncognitasSalas[Sala - 1]){
+            EstadoJuego++;
+            return true;
         }
+        else
+            return false;
+        
     }
 }
